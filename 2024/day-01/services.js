ef9124d7f67c187444e9e7dbc30919f512c427ca
Laureline <laurelinedev@gmail.com>
2024-12-02 20:30:39 +0100

    Day 02

--- 2024/day-01/services.js
+++ 2024/day-01/services.js
@@ -1,26 +1,33 @@
-const { readFile } = require('node:fs/promises')
+const { readFile } = require('node:fs/promises');
+const { useInputPath } = require('../../utils');
+
+ const INPUT_FILE = useInputPath(__dirname)
 
 
-async function delimitLeftFromRight ( file ){
+const formatInputData = data => {
+	const splittedLines = data
+		// 1. gets lines
+		.split(/\n/g)
+
+		// 2. gets and format values per lines
+		.map(lineStr => lineStr
+			.split(/\s+/)
+			.map( Number)
+		)
+
+		// 3. gets 2 arrays from left values and right values
+		.reduce(( acc, [colVal1, colVal2 ]) => {
+			acc[0].push( colVal1 );
+			acc[1].push( colVal2 );
+			return acc;
+		}, [[], []])
+	return splittedLines
+}
+async function getInputData (){
 	try {
-		const content = await readFile( file, 'utf-8' )
-		const linesSplitted = content
-			// 1. gets lines
-			.split(/\n/g)
-
-			// 2. gets and format values per lines
-			.map(lineStr => lineStr
-				.split(/\s+/)
-				.map( Number)
-			)
-
-			// 3. gets 2 arrays from left values and right values
-			.reduce(( acc, [colVal1, colVal2 ]) => {
-				acc[0].push( colVal1 );
-				acc[1].push( colVal2 );
-				return acc;
-			}, [[], []])
-		return linesSplitted
+		const content = await readFile( INPUT_FILE, 'utf-8' )
+		const dataFormatted = formatInputData( content )
+		return dataFormatted
 
 	} catch (error) {
 		console.error('A problem occurred')
@@ -30,7 +37,6 @@ async function delimitLeftFromRight ( file ){
 
 
 
-
 module.exports = {
-	delimitLeftFromRight
+	getInputData
 }
