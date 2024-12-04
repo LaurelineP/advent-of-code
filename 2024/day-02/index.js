a7ae2107d017222ce16e96fcbd1bff7fdc83fcc8
Laureline <laurelinedev@gmail.com>
2024-12-04 15:43:24 +0100

    Refacto day 02

--- 2024/day-02/index.js
+++ 2024/day-02/index.js
@@ -1,59 +1,9 @@
-const { getInputData, isDeltaIncluded } = require('./services')
+const { getInputData, isSaveRow, isSaveable } = require('./services')
 const { logValue, watchError } = require('../../core/tools')
 
 
 let edgeCases = []
 let totalSaved = 0
-const getRowOrder = row => {
-	const rowOrderRef = row.toString()
-	const rowOrderInc = [...row].sort((a,b) => a - b).toString()
-	const rowOrderDec = [...row].sort((a,b) => b - a).toString()
-
-	return rowOrderRef === rowOrderInc ? 'inc'
-		: rowOrderRef === rowOrderDec ? 'dec'
-		: 'unordered'
-}
-
-
-// Checks (report) if "safe" ( delta being between 1 - 3 for following row's value )
-const isSaveRow = row => {
-	const _isSaveRow = row.every(( value, idx ) => {
-		const prev = row[idx - 1]
-		const next = row[idx + 1]
-	
-		const hasNext = !isNaN( next )
-
-		/* ----------------------------- BASIC SAFE ROW ----------------------------- */
-		// Controller of delta value based on order
-		const orderController = {
-			inc: hasNext ? (next - value) : (value - prev),
-			dec: hasNext ? (value - next) : (prev - value),
-		}
-
-		// Handles row's value order - check if values are following sequence
-		const order = getRowOrder( row )
-		const delta = orderController[ order ] || null
-
-		return !!delta && isDeltaIncluded( delta )
-	})
-	return _isSaveRow
-}
-
-
-const isSaveable = row => {
-	const isNotEligible = row.length < 2
-	if(isNotEligible) return false
-
-	// Found value -> indicates the row is safe without that value
-	const saveableRow = row.find((_, idx) => {
-		const temp = [...row].toSpliced(idx, 1)
-		const isSafeRowCandidate = isSaveRow(temp)
-		return isSafeRowCandidate
-	})
-	
-	return !!saveableRow
-}
-
 
 const runPartOne = data => {
 	try {
--- 2024/day-02/services.js
+++ 2024/day-02/services.js
@@ -8,6 +8,60 @@ const INCLUDED_DELTAS = [ 1, 2, 3 ]
 
 const isDeltaIncluded = d => INCLUDED_DELTAS.includes(d)
 
+const getRowOrder = row => {
+	const rowOrderRef = row.toString()
+	const rowOrderInc = [...row].sort((a,b) => a - b).toString()
+	const rowOrderDec = [...row].sort((a,b) => b - a).toString()
+
+	return rowOrderRef === rowOrderInc ? 'inc'
+		: rowOrderRef === rowOrderDec ? 'dec'
+		: 'unordered'
+}
+
+
+// Checks (report) if "safe" ( delta being between 1 - 3 for following row's value )
+const isSaveRow = row => {
+	const _isSaveRow = row.every(( value, idx ) => {
+		const prev = row[idx - 1]
+		const next = row[idx + 1]
+	
+		const hasNext = !isNaN( next )
+
+		/* ----------------------------- BASIC SAFE ROW ----------------------------- */
+		// Controller of delta value based on order
+		const orderController = {
+			inc: hasNext ? (next - value) : (value - prev),
+			dec: hasNext ? (value - next) : (prev - value),
+		}
+
+		// Handles row's value order - check if values are following sequence
+		const order = getRowOrder( row )
+		const delta = orderController[ order ] || null
+
+		return !!delta && isDeltaIncluded( delta )
+	})
+	return _isSaveRow
+}
+
+
+const isSaveable = row => {
+	const isNotEligible = row.length < 2
+	if(isNotEligible) return false
+
+	// Found value -> indicates the row is safe without that value
+	const saveableRow = row.find((_, idx) => {
+		const temp = [...row].toSpliced(idx, 1)
+		const isSafeRowCandidate = isSaveRow(temp)
+		return isSafeRowCandidate
+	})
+	
+	return !!saveableRow
+}
+
+
+
+
+
 
 const formatInputData = content => {
 	if( !content ) return content
@@ -32,5 +86,7 @@ const getInputData = async () => {
 
 module.exports = {
 	getInputData,
-	isDeltaIncluded
+	isDeltaIncluded,
+	isSaveRow,
+	isSaveable
 }
